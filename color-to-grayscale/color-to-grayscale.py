2a8b9b58a15556177155558553fc495bbe83b414
Vedaang Chopra <32367951+Vedaang-Chopra@users.noreply.github.com>
2026-03-31 14:17:00 -0400

    Add solution for color-to-grayscale

---
+++ color-to-grayscale/color-to-grayscale.py
@@ -0,0 +1,9 @@
+import numpy as np
+def color_to_grayscale(image):
+    """
+    Convert an RGB image to grayscale using luminance weights.
+    """
+    # Write code here
+    image = np.array(image)
+    print(image[:, :0], image[:, :, 1], image[: , :, 2])
+    return( 0.299 * image[:, :, 0] + 0.587 * image[:, :, 1] + 0.114 * image[:, :, 2]).tolist()
